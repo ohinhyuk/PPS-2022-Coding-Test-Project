9944637e3c6a7f33c7508de64a16015ba0bea1e4
ohinhyuk <8156217@naver.com>
2022-07-15 15:04:01 +0900

    commit

--- 0715_Day12/A030_오인혁_20220715.cpp
+++ 0715_Day12/A030_오인혁_20220715.cpp
@@ -1,23 +1,39 @@
+// BeakJun 17211
+// Title : 좋은 날 싫은 날
+// URL : https://www.acmicpc.net/problem/17211
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void){
 
-    int N , feeling;
-    float percent[4];
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-    float good;
-    
-    cin >> N >> feeling;
+    int N;
+    bool bad;
+    float percent [4];
+
+    cin >> N >> bad;
+    float answer[N][2];
+
     for(int i = 0 ; i < 4 ; ++i) cin >> percent[i];
 
-    if(N == 1){
-        if(feeling == 0) cout << percent[0] * 1000 << "\n" << 1000 - percent[0] * 1000;
-        else cout << percent[2] * 1000 << "\n" << 1000 - percent[2]*1000;
+    
+    if(bad) answer[0][0] = percent[2]* 1000;
+    else answer[0][0] = percent[0] * 1000;
+        
+    answer[0][1] = 1000 - answer[0][0];
+
+    if(N >= 2){
+        for(int i = 1 ; i < N ; ++i){
+            answer[i][0] = answer[i-1][0] * percent[0] + answer[i-1][1] * percent[2];
+            answer[i][1] = answer[i-1][0] * percent[1] + answer[i-1][1] * percent[3];
+        }
     }
-    else{
-        if(feeling == 0){ good = (percent[0] * percent[0] + percent[1] * percent[2])* 1000 ; cout << good << '\n' << 1000-good ;}
-        else {good = (percent[3] * percent[2] + percent[2] * percent[0] )* 1000; cout << good << '\n' << 1000-good ;}
-    }
+    cout << fixed;
+    cout.precision(0);
+    cout << answer[N-1][0] << "\n" << answer[N-1][1];
 }
